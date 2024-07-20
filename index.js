2732733661c3284cae3971cbb6f3d00107655f49
Michael Chandler <macjr93@yahoo.com>
2024-07-19 22:39:28 -0700

    setup dieRoller page basics

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const d4 = require('./dice-mechanics/diceForms/d4');
 
 // set to ejs, fix ejs pathing
 app.set('view engine', 'ejs');
@@ -10,6 +11,11 @@ app.set('views', path.join(__dirname, '/views'));
 // home route
 app.get('/', (req, res) => {
     res.render('home.ejs');
+});
+
+app.get('/dieRoller', (req, res) => {
+    const { d4 } = d4;
+    res.render('diRoller.ejs', { d4 });
 });
 
 // Default route
